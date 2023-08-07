4e5890313c0485c17518bfb9880ddd99a6062137
IrynaDidkivska <ididkivska@gmail.com>
2023-08-07 18:35:09 +0200

    used hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
+import { toast } from 'react-toastify';
+import React, { useState, useEffect } from 'react';
+import { ContactList } from './ContactsList/ContactList';
+import { ContactForm } from './ContactsForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { Title, Wrapper } from './App.styled';
+import { PrevContacts } from './PrevContacts/PrevContacts';
+
+// const prevContacts = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
+
 export const App = () => {
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  //Додаємо новий контакт
+  const addNewContact = newContact => {
+    const checkContact = contacts.find(
+      contact => contact.name === newContact.name
+    );
+    if (checkContact) {
+      toast.warning(`${newContact.name} is already in contacts`);
+    } else {
+      setContacts(prev => [...prev, newContact]);
+    }
+  };
+
+  //вводимо в інтуп фільтра
+  const hendleFilterInput = value => {
+    setFilter(value);
+  };
+  //фільтруємо
+  const getFilteredContact = () => {
+    if (!filter) {
+      return contacts;
+    }
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+  //видаляємо
+  const hendleDeleteContact = (id, name) => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
+    toast.success(`Контакт ${name} успішно видалено!`);
+  };
+
+  const filteredContact = getFilteredContact(filter);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Wrapper>
+      <Title>Phonebook</Title>
+      <ContactForm addNewContact={addNewContact} />
+      <h2>Contacts</h2>
+
+      <Filter filter={filter} onFilterInput={hendleFilterInput} />
+
+      <ContactList contacts={filteredContact} onDelete={hendleDeleteContact} />
+      <PrevContacts />
+    </Wrapper>
   );
 };
---
+++ src/components/PrevContacts/PrevContacts.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button, Contact, List } from './PrevContact.styled';
+
+export const PrevContacts = () => {
+  const prevContacts = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+  return (
+    <div>
+      <List>
+        {prevContacts.map(contact => (
+          <Contact key={contact.id}>
+            <p>{contact.name} : </p>
+            <p>{contact.number}</p>
+            <Button>Saved</Button>
+          </Contact>
+        ))}
+      </List>
+    </div>
+  );
+};
